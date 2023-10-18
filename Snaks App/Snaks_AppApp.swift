63663e64f2af06c49a71ed4880c711d7d7fec5d9
Ramia-aloufi <ramia.aloufi@gmail.com>
2023-10-18 14:21:53 +0300

    Initial Commit

---
+++ Snaks App/Snaks_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snaks_AppApp.swift
+//  Snaks App
+//
+//  Created by ramia n on 03/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Snaks_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
